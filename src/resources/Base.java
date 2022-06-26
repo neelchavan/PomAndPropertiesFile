b46272a636a478f87c93eb7888cb3d59b7a20b2f
neelchavan <neelchavan9@gmail.com>
2022-06-26 16:05:53 +0530

    Added important comments

--- src/resources/Base.java
+++ src/resources/Base.java
@@ -9,24 +9,28 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 public class Base {
 
 	public WebDriver driver;
-	public static String browserName;
+
+	// define variable browserName
+	public static String browserName = null;
 
 	public WebDriver startDriver() {
+		// call static method getProperties() from PropertiesFile class this will set
+		// the browserName.
 		PropertiesFile.getProperties();
+
 		System.out.println("All the test cases will now run on " + browserName);
+
+		// opens the Browser according to the browser name specified in config file
 		if (browserName.equals("firefox")) {
 			System.setProperty("webdriver.gecko.driver", "D:\\Neel\\tools\\gecko\\geckodriver.exe");
 			driver = new FirefoxDriver();
-			driver.manage().window().maximize();
-			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(8));
 		} else if (browserName.equals("chrome")) {
 			System.setProperty("webdriver.chrome.driver", "D:\\Neel\\tools\\chromeD\\chromedriver.exe");
 			driver = new ChromeDriver();
-			driver.manage().window().maximize();
-			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(8));
 		}
 
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(8));
 		return driver;
 	}
-
 }
--- src/resources/PropertiesFile.java
+++ src/resources/PropertiesFile.java
@@ -1,27 +1,31 @@
 package resources;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.util.Properties;
 
 public class PropertiesFile {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		getProperties();
-	}
-
 	public static void getProperties() {
 
 		try {
 			Properties prop = new Properties();
+
+			// get current project path
 			String projectPath = System.getProperty("user.dir");
-			System.out.println(projectPath);
+
+			// Create a inputStream to read from a file
 			InputStream input = new FileInputStream(projectPath + "\\src\\resources\\config.properties");
+
+			// load the config file to get the key value pairs.
 			prop.load(input);
+
+			// read the config file
 			String browser = prop.getProperty("browser");
+
+			// Set the browserName in base class to browser name specified in config file
 			Base.browserName = browser;
+
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 			System.out.println(e.getCause());
